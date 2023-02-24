38809d90a8598d26568c6dbef768f4be5e1c4919
angelrm123 <ar595409@gmail.com>
2023-02-24 12:02:15 -0600

    ejercicio1

--- ProjectJavaAYRM/src/main/java/org/example/Main.java
+++ ProjectJavaAYRM/src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main {    public static void main(String[] args) {
+
+    /*System.out.println("Hello world!");*/
+    int a =10;
+    int b =5;
+    int c=-3;
+
+    if(a>b){
+        System.out.println("a es mayor que b");
+
+    }else{
+        System.out.println("a no es mayor que b");
+    }
+
+
+
     }
 }
+
+
+
+
---
+++ Python/ejercicio1.py
@@ -0,0 +1,10 @@
+a = 54
+b = 69
+c = 3
+
+if a > b:
+       print("a es mayor que b")
+
+else:
+        print("a no es mayor que b") 
+ 
