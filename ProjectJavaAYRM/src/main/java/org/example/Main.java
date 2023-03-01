ed5c7f4c4c3a9090ff584fd1153f630f385ec38f
angelrm123 <ar595409@gmail.com>
2023-03-01 10:40:15 -0600

    Ejercicio 2

--- ProjectJavaAYRM/src/main/java/org/example/Main.java
+++ ProjectJavaAYRM/src/main/java/org/example/Main.java
@@ -1,22 +1,67 @@
 package org.example;
 
-public class Main {    public static void main(String[] args) {
+import java.util.Scanner;
 
-    /*System.out.println("Hello world!");*/
-    int a =10;
-    int b =5;
-    int c=-3;
+public class Main {
+    public static void main(String[] args) {
 
-    if(a>b){
-        System.out.println("a es mayor que b");
+        //Desarrollar un programa que le pregunte al
+        // usuario 3 numeros  y decirle cual es el mayor de los 3.
+       /* Scanner scanner = new Scanner(System.in);
 
-    }else{
-        System.out.println("a no es mayor que b");
+        System.out.println("Ingresa 4 numeros");
+
+        double numero1, numero2, numero3, numero4;
+        numero1 = scanner.nextDouble();
+        numero2 = scanner.nextDouble();
+        numero3 = scanner.nextDouble();
+        numero4 = scanner.nextDouble();
+
+
+        if (numero1 > numero2 && numero1 > numero3 && numero1> numero4) {
+            System.out.println("El numero1 es mayor ");
+        }
+        if (numero2 > numero3 && numero2 > numero1 && numero2> numero4) {
+            System.out.println("El numero2 es mayor");
+        }
+        if (numero3 > numero2 && numero3 > numero1 && numero3> numero4) {
+            System.out.println("El numero3 es mayor");
+        }
+        if (numero1 == numero2 && numero1 == numero3 && numero1 == numero4 ) {
+            System.out.println("Los numeros son iguales");
+        }
+
+
+*/
+for(int i=0; i<10; i++){
+    System.out.println("i vale: "+i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
     }
 
-
-
-    }
 }
 
 
---
+++ Python/ejercicio2.py
@@ -0,0 +1,9 @@
+numero = input("Ingrese un numero: ")
+numero = int(numero)
+
+print(numero)
+if numero > 100 and numero < 1000000:
+    print("Correcto")
+
+else:
+    print("Error")
