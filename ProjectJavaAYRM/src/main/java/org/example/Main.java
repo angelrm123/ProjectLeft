4e307f2bbbca48f7921df5d6a5229760ecdc1735
angelrm123 <ar595409@gmail.com>
2023-03-02 12:53:08 -0600

    practica5

--- ProjectJavaAYRM/src/main/java/org/example/Main.java
+++ ProjectJavaAYRM/src/main/java/org/example/Main.java
@@ -32,9 +32,53 @@ public class Main {
         }
 
 
-*/
+
 for(int i=0; i<10; i++){
-    System.out.println("i vale: "+i);
+    System.out.println("i vale: "+i);*/
+//Suma de 2 numeros y que se repita 4 veces
+        /*Scanner miscaner = new Scanner(System.in);
+        System.out.println("Ingresa un numero");
+       int numero1;
+       int numero2;
+       int suma;
+
+        for(int i=0; i<4; i++){
+
+
+            System.out.println("Ingresa el primer numero: ");
+            numero1 = miscaner.nextInt();
+
+            System.out.print("Ingresa el segundo numero: ");
+            numero2 = miscaner.nextInt();
+
+
+            suma = numero1 + numero2;
+            System.out.print("El resultado de la suma es: " + suma);
+
+        }*/
+
+        /*Scanner miscaner = new Scanner(System.in);
+
+        int edad;
+
+
+        for(int i=0; i<4; i++){
+            System.out.println("Ingrese su edad");
+            edad = miscaner.nextInt();
+
+            System.out.println();
+            if (edad > 17){
+                System.out.println("Es mayor de edad");
+            }else{
+                System.out.println("Es menor de edad");
+
+
+            }
+        }*/
+
+// Doublee
+
+        //miArrayDouble[] = new double[7];
 
 
 
@@ -56,7 +100,12 @@ for(int i=0; i<10; i++){
 
 
 
-}
+                      
+
+
+
+
+
 
 
 
---
+++ Python/ejercicio3.py
@@ -0,0 +1,11 @@
+# Pedir al usuario su nombre su edad y precio de su carro y calcular la tendencia de 1.3%
+
+precoCarro = input("Ingresa el precio de tu carro")
+precioCarro = float (precioCarro)
+
+precioCarro = 0.013
+
+inpustoTendencia = 0.013
+precioTendencia = PrecioCarro * inpuestoTendencia
+print ("Precio tendencia: ",  precioTendencia)
+
